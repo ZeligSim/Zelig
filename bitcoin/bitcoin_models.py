457799d86cf973ae1f94bae649e14c1b621b9607
egeerdogan <ege@erdogan.dev>
2021-05-15 21:12:23 +0300

    logging

--- bitcoin/bitcoin_models.py
+++ bitcoin/bitcoin_models.py
@@ -10,6 +10,7 @@ from loguru import logger
 from sim.base_models import *
 from messages import InvMessage, GetDataMessage, GetBlockchainMessage
 
+logger.add('bitcoin_logs.txt')
 
 class Block(Item):
     def __init__(self, miner: Node, sender_id: str, timestamp: int, created_at: int, prev_id: str):
@@ -24,13 +25,14 @@ class Block(Item):
 
 # TODO: check timestamp logic and links (not) sending items
 class Miner(Node):
-    def __init__(self, pos_x: float, pos_y: float, mine_power: int, mine_cost=0, timestamp=0):
+    def __init__(self, name: str, pos_x: float, pos_y: float, mine_power: int, mine_cost=0, timestamp=0):
         super().__init__(pos_x, pos_y, timestamp)
+        self.name = name
         self.mine_power = mine_power
         self.mine_cost = mine_cost
         self.blockchain: List[Block] = []
         self.blockpool: Dict[str, Block] = dict()
-        logger.info(f'Created miner {self.id}')
+        logger.info(f'Created miner {self.name}')
 
     def step(self):
         super().step()
@@ -49,23 +51,29 @@ class Miner(Node):
 
     def __consume(self, item: Item):
         if type(item) == Block:
-            print('hello there')
+            logger.info(f'[{self.timestamp}] {self.name} RECEIVED BLOCK {item.id}')
+            self.blockpool[item.id] = item
             self.__publish_block(item)
             if item.prev_id == self.blockchain[-1].id:
                 self.blockchain.append(item)
         elif type(item) == InvMessage:
+            logger.info(f'[{self.timestamp}] {self.name} RECEIVED INV MESSAGE FOR BLOCK {item.block_id}')
             if item.block_id not in self.blockpool.keys():
+                logger.info(f'[{self.timestamp}] {self.name} RESPONDED WITH GETDATA')
                 msg = GetDataMessage(item.block_id, self.id, self.timestamp, 10, self.timestamp)
                 self.__send_to(item.sender_id, msg)
         elif type(item) == GetDataMessage:
+            logger.info(f'[{self.timestamp}] {self.name} RECEIVED GETDATA MESSAGE FOR BLOCK {item.block_id}')
             self.__send_to(item.sender_id, self.blockpool[item.block_id])
         elif type(item) == GetBlockchainMessage:
             for block in self.blockchain:
                 self.__send_to(item.sender_id, block)
 
+    # FIXME: made public for debugging
     def generate_block(self):
         prev = self.__choose_prev_block()
         block = Block(self, self.id, self.timestamp, self.timestamp, prev.id)
+        logger.info(f'[{self.timestamp}] {self.name} GENERATED BLOCK {block.id}')
         self.blockchain.append(block)  # TODO:
         self.blockpool[block.id] = block
         self.__publish_block(block)
--- bitcoin/main.py
+++ bitcoin/main.py
@@ -1,21 +1,32 @@
 from bitcoin_models import Block, Miner
 
-miner_a = Miner(0, 0, 1000)
-miner_b = Miner(100, 100, 1000)
 
-miner_a.connect(miner_b)
-miner_b.connect(miner_a)
+def connect(m1: Miner, m2: Miner):
+    m1.connect(m2)
+    m2.connect(m1)
+
+
+SIMULATION_TIME = 40
+
+miner_a = Miner('MINER_A', 0, 0, 1000)
+miner_b = Miner('MINER_B', 100, 100, 1000)
+miner_c = Miner('MINER_C', 200, 200, 1000)
+miner_d = Miner('MINER_D', 300, 300, 1000)
+
+connect(miner_a, miner_b)
+connect(miner_b, miner_c)
+connect(miner_c, miner_d)
+
+nodes = [miner_a, miner_b, miner_c, miner_d]
 
 genesis_block = Block('satoshi', 'satoshi', 0, 0, 'satoshi')
-miner_a.blockchain.append(genesis_block)
-miner_b.blockchain.append(genesis_block)
 
-nodes = [miner_a, miner_b]
-time = 0
-while True:
-    time += 1
+for node in nodes:
+    node.blockchain.append(genesis_block)
+
+for time in range(1, SIMULATION_TIME):
     for node in nodes:
         node.step()
-    # x = input()
-    if time % 10 == 0:
+    if time == 5:
         miner_a.generate_block()
+
