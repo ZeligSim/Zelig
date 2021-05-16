8568ba95eb525be3823e9bdd64e63b3299252d9a
egeerdogan <ege@erdogan.dev>
2021-05-16 11:02:44 +0300

    miners mine on longest chain when fork occurs

--- bitcoin/bitcoin_models.py
+++ bitcoin/bitcoin_models.py
@@ -8,9 +8,10 @@ sys.path.append("..")
 from loguru import logger
 
 from sim.base_models import *
-from messages import InvMessage, GetDataMessage, GetBlockchainMessage
+from messages import InvMessage, GetDataMessage
 
 logger.add('bitcoin_logs.txt')
+
 
 class Block(Item):
     def __init__(self, miner: Node, sender_id: str, timestamp: int, created_at: int, prev_id: str):
@@ -20,7 +21,7 @@ class Block(Item):
         self.miner = miner
 
     def __str__(self) -> str:
-        return f'Block: {self.id}'
+        return f'BLOCK (id:{self.id}, prev: {self.prev_id})'
 
 
 # TODO: check timestamp logic and links (not) sending items
@@ -30,9 +31,9 @@ class Miner(Node):
         self.name = name
         self.mine_power = mine_power
         self.mine_cost = mine_cost
-        self.blockchain: List[Block] = []
-        self.blockpool: Dict[str, Block] = dict()
-        logger.info(f'Created miner {self.name}')
+        self.blockchain: Dict[str, Block] = dict()
+        self.heads: List[Block] = []
+        logger.info(f'CREATED MINER {self.name}')
 
     def step(self):
         super().step()
@@ -52,40 +53,46 @@ class Miner(Node):
     def __consume(self, item: Item):
         if type(item) == Block:
             logger.info(f'[{self.timestamp}] {self.name} RECEIVED BLOCK {item.id}')
-            self.blockpool[item.id] = item
+            self.add_block(item)
             self.__publish_block(item)
-            if item.prev_id == self.blockchain[-1].id:
-                self.blockchain.append(item)
         elif type(item) == InvMessage:
             logger.info(f'[{self.timestamp}] {self.name} RECEIVED INV MESSAGE FOR BLOCK {item.block_id}')
-            if item.block_id not in self.blockpool.keys():
+            if item.block_id not in self.blockchain.keys():
                 logger.info(f'[{self.timestamp}] {self.name} RESPONDED WITH GETDATA')
                 msg = GetDataMessage(item.block_id, self.id, self.timestamp, 10, self.timestamp)
                 self.__send_to(item.sender_id, msg)
         elif type(item) == GetDataMessage:
             logger.info(f'[{self.timestamp}] {self.name} RECEIVED GETDATA MESSAGE FOR BLOCK {item.block_id}')
-            self.__send_to(item.sender_id, self.blockpool[item.block_id])
-        elif type(item) == GetBlockchainMessage:
-            for block in self.blockchain:
-                self.__send_to(item.sender_id, block)
+            self.__send_to(item.sender_id, self.blockchain[item.block_id])
 
-    # FIXME: made public for debugging
-    def generate_block(self):
-        prev = self.__choose_prev_block()
+    # FIXME: public for testing
+    def generate_block(self, prev=None) -> Block:
+        if prev is None:
+            prev = self.__choose_prev_block()
         block = Block(self, self.id, self.timestamp, self.timestamp, prev.id)
         logger.info(f'[{self.timestamp}] {self.name} GENERATED BLOCK {block.id}')
-        self.blockchain.append(block)  # TODO:
-        self.blockpool[block.id] = block
+        self.add_block(block)
         self.__publish_block(block)
+        return block
+
+    # FIXME: public for testing
+    # remove block.prev from heads if exists and add block to blockchain
+    def add_block(self, block):
+        self.blockchain[block.id] = block
+        prev = list(filter(lambda head: head.id == block.prev_id, self.heads))
+        if len(prev) > 0:
+            self.heads.remove(prev[0])
+        self.heads.append(block)
 
     def __publish_block(self, block: Block):
         for link in self.outs:
             msg = InvMessage(block.id, self.id, self.timestamp, 10, self.timestamp)
             link.send(msg)
 
-    # TODO: implements the main consensus logic to choose which block to mine on
+    # returns the head of the longest chain TODO
     def __choose_prev_block(self) -> Block:
-        return self.blockchain[-1]
+        lengths = [self.__get_length(block) for block in self.heads]
+        return self.heads[lengths.index(max(lengths))]
 
     def __send_to(self, node_id: str, item: Item):
         for link in self.outs:
@@ -94,3 +101,21 @@ class Miner(Node):
 
     def __get_difficulty(self) -> float:
         return 2 ** 2 / 2 ** 256
+
+    # get length of chain starting ending at `block`
+    def __get_length(self, block, count=0):
+        prev = self.blockchain.get(block.prev_id, None)
+        if prev is not None:
+            return self.__get_length(prev, count + 1)
+        else:
+            return count
+
+    # -- LOGGING / INFO METHODS --
+    def log_blockchain(self):
+        logger.info(f'{self.name}')
+        logger.info(f'\tBLOCKCHAIN:')
+        for block in self.blockchain.values():
+            logger.info(f'\t\t{block}')
+        logger.info(f'\tHEADS:')
+        for block in self.heads:
+            logger.info(f'\t\t{block}')
--- bitcoin/main.py
+++ bitcoin/main.py
@@ -6,7 +6,7 @@ def connect(m1: Miner, m2: Miner):
     m2.connect(m1)
 
 
-SIMULATION_TIME = 40
+SIMULATION_TIME = 200
 
 miner_a = Miner('MINER_A', 0, 0, 1000)
 miner_b = Miner('MINER_B', 100, 100, 1000)
@@ -22,11 +22,21 @@ nodes = [miner_a, miner_b, miner_c, miner_d]
 genesis_block = Block('satoshi', 'satoshi', 0, 0, 'satoshi')
 
 for node in nodes:
-    node.blockchain.append(genesis_block)
+    node.add_block(genesis_block)
 
 for time in range(1, SIMULATION_TIME):
     for node in nodes:
         node.step()
     if time == 5:
-        miner_a.generate_block()
+        a_block = miner_a.generate_block()
+    if time == 40:
+        aa_block = miner_a.generate_block()
+    if time == 80:
+        c_block = miner_c.generate_block(prev=genesis_block)
+    if time == 120:
+        a_block = miner_a.generate_block()
+
+
+for miner in nodes:
+    miner.log_blockchain()
 
